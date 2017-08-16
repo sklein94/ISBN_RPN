16eafae86bfec82725443efe9292c62d044d75ad
sklein94 <simon.klein.sz.1@gmail.com>
2017-08-16 14:44:15 +0200

    Added Comments in SquaringTest

--- src/test/java/Calculate/SquaringTest.java
+++ src/test/java/Calculate/SquaringTest.java
@@ -16,14 +16,18 @@ public class SquaringTest implements OperationTest{
 
     @Test
     public void shouldBeCorrect() throws Exception {
+        //Normale Quadrierung
         this.squaringOfValueIsCorrect("-100", "10000");
         this.squaringOfValueIsCorrect("-1", "1");
         this.squaringOfValueIsCorrect("0", "0");
         this.squaringOfValueIsCorrect("1", "1");
         this.squaringOfValueIsCorrect("100", "10000");
 
+        //Quadrierung mit Kommazahl
         this.squaringOfValueIsCorrect("1.5", "2.25");
+//        this.squaringOfValueIsCorrect("0.3333333333", "0.1111111111");
 
+        //Quadrierung mit sehr niedrigen und sehr hohen Werten
         this.squaringOfValueIsCorrect("1E-100", "1E-200");
         this.squaringOfValueIsCorrect("1E+100", "1E+200");
     }
