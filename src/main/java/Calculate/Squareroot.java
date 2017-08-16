9bc765a3489656d5f7dcff1bae242d6fa6c1855f
sklein94 <simon.klein.sz.1@gmail.com>
2017-08-16 11:27:07 +0200

    Finished all Operations and Wrote Tests for them, but tests need to be improved by periodic values Implementation of sqrt and pow are also incorrect after certain decimal places

--- src/main/java/Calculate/Squareroot.java
+++ src/main/java/Calculate/Squareroot.java
@@ -1,67 +1,26 @@
-//package Calculate;
-//
-//import Exceptions.NumberOfParametersException;
-//
-//import java.math.BigDecimal;
-//
-//public class Squareroot implements Operation {
-//    static {
-//        Operators.listOfOperators.add("sqrt");
-//    }
-//
-//    public BigDecimal calculate(BigDecimal... arr) throws Exception {
-//        if (arr.length == 1) {
-//            return this.sqrt(arr[0]);
-//        }
-//        else {
-//            throw new NumberOfParametersException("Parameters: " + arr.length);
-//        }
-//    }
-//
-//   /* private BigDecimal sqrt(BigDecimal argument) {
-//        int count = 0;
-//        BigDecimal increment = BigDecimal.ONE;
-//        BigDecimal value = increment;
-//        while (!this.multipliedEquals(value, argument) && count < 1000) {
-//            if (this.isMultipliedBiggerThan(value, argument)){
-//
-//            }
-//            count++;
-//        }
-//        return value;
-//
-//    }
-//
-//    private boolean multipliedEquals(BigDecimal value, BigDecimal valueToCheck){
-//        return value.multiply(value).compareTo(valueToCheck) == 0;
-//    }
-//
-//    private boolean isMultipliedBiggerThan(BigDecimal value, BigDecimal valueToCheck) {
-//        BigDecimal result = value.multiply(value);
-//        return result.compareTo(valueToCheck) == 1;
-//    }
-//
-//    private boolean isMultipliedBiggerThan(BigDecimal value, BigDecimal valueToCheck) {
-//        BigDecimal result = value.multiply(value);
-//        return result.compareTo(valueToCheck) == 1;
-//        BigDecimal.sq
-//    }
-//
-//    private boolean isWithIncrementMultipliedBiggerThan(BigDecimal value, BigDecimal increment, BigDecimal valueToCheck) {
-//        BigDecimal result = value.add(increment).multiply(value.add(increment));
-//        return result.compareTo(valueToCheck) == 1;
-//    }
-//
-//    private boolean isWithDeductedIncrementMultipliedSmallerThan(BigDecimal value, BigDecimal increment, BigDecimal valueToCheck) {
-//        BigDecimal result = value.subtract(increment).subtract(value.add(increment));
-//        return result.compareTo(valueToCheck) == -1;
-//    }
-//
-//    private BigDecimal sqr(BigDecimal argument) {
-//        return argument.multiply(argument);
-//    }
-//
-//    public boolean canCalculate(String operator) {
-//        return operator.equals("sqrt");
-//    }*/
-//}
+package Calculate;
+
+import Exceptions.NumberOfParametersException;
+import ch.obermuhlner.math.big.BigDecimalMath;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class Squareroot implements Operation {
+    static {
+        Operators.listOfOperators.add("sqrt");
+    }
+
+    public BigDecimal calculate(BigDecimal... arr) throws Exception {
+        if (arr.length == 1) {
+            return BigDecimalMath.sqrt(arr[0], MathContext.UNLIMITED);
+        }
+        else {
+            throw new NumberOfParametersException("Parameters: " + arr.length);
+        }
+    }
+
+    public boolean canCalculate(String operator) {
+        return operator.equals("sqrt");
+    }
+}
--- src/test/java/Calculate/SquarerootTest.java
+++ src/test/java/Calculate/SquarerootTest.java
@@ -1,93 +1,97 @@
-//package Calculate;
-//
-//import Exceptions.NumberOfParametersException;
-//import org.junit.Test;
-//
-//import java.math.BigDecimal;
-//
-//import static org.junit.Assert.*;
-//
-//public class SquarerootTest implements OperationTest{
-//
-//    @Test
-//    public void shouldBeCorrect() throws Exception {
-//        this.calculationOf("4", "2");
-//        this.calculationOf("2.25", "1.5");
-//        this.calculationOf("0", "0");
-//    }
-//
-//    @Test
-//    public void notNegativeValue(){
-//        this.failOnThisArgument("-1");
-//        this.failOnThisArgument("-0.25");
-//    }
-//
-//    @Test
-//    public void notEnoughParameters() throws Exception {
-//        this.failOnThisNumberOfArguments();
-//    }
-//
-//    @Test
-//    public void tooManyParameters() throws Exception {
-//        this.failOnThisNumberOfArguments(new BigDecimal(1),new BigDecimal(1));
-//        this.failOnThisNumberOfArguments(new BigDecimal(1),new BigDecimal(1), new BigDecimal(1));
-//        this.failOnThisNumberOfArguments(new BigDecimal(1),new BigDecimal(1), new BigDecimal(1), new BigDecimal(1));
-//    }
-//
-//    @Test
-//    public void operatorShouldBeValid() throws Exception {
-//        checkOperatorToCalculate("sqrt", true);
-//    }
-//
-//    @Test
-//    public void operatorShouldBeInvalid() throws Exception {
-//        for (String temp : Operators.listOfOperators){
-//            if (!temp.equals("sqrt")){
-//                checkOperatorToCalculate(temp, false);
-//            }
-//        }
-//    }
-//
-//    private void failOnThisArgument(String argument){
-//        Squareroot squareroot = new Squareroot();
-//        BigDecimal argumentAsBigDecimal = new BigDecimal(argument);
-//        try {
-//            squareroot.calculate(argumentAsBigDecimal);
-//            fail("Es wurde ein falsches Argument akzeptiert.");
-//        }
-//        catch (Exception e) {
-//
-//        }
-//    }
-//
-//    public void failOnThisNumberOfArguments(BigDecimal... arguments) throws Exception {
-//        Squareroot squareroot = new Squareroot();
-//        try {
-//            squareroot.calculate(arguments);
-//            fail("Es wurde eine falsche Anzahl an Parametern akzeptiert.");
-//        }
-//        catch (NumberOfParametersException numberOfParameterException) {
-//
-//        }
-//    }
-//
-//    public void calculationOf(String... arguments) throws Exception {
-//        Squareroot squareroot = new Squareroot();
-//        BigDecimal calculate = squareroot.calculate(new BigDecimal(arguments[0]));
-//        BigDecimal expected = new BigDecimal(arguments[1]);
-//        String message = "\nExpected: " + expected.toString() + "\nActual: " + calculate.toString();
-//
-//        assertEquals(message, 0, calculate.compareTo(expected));
-//    }
-//
-//    public void checkOperatorToCalculate(String operator, boolean shouldBeCorrect) throws Exception {
-//        Squareroot squareroot = new Squareroot();
-//        String message = "Operator: " + operator;
-//        if (shouldBeCorrect){
-//            assertTrue(message, squareroot.canCalculate(operator));
-//        }
-//        else{
-//            assertFalse(message, squareroot.canCalculate(operator));
-//        }
-//    }
-//}
+package Calculate;
+
+import Exceptions.NumberOfParametersException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+public class SquarerootTest implements OperationTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldBeCorrect() throws Exception {
+        this.SquarerootOfValuesIsCorrect("4", "2");
+        this.SquarerootOfValuesIsCorrect("268435456", "16384");
+
+        this.SquarerootOfValuesIsCorrect("2.25", "1.5");
+        //this.SquarerootOfValuesIsCorrect("1.78", "1.33416640641");
+
+        this.SquarerootOfValuesIsCorrect("0", "0");
+    }
+
+    @Test
+    public void invalidArguments() throws Exception {
+        this.failOnThisArgument("-1");
+        this.failOnThisArgument("-0.255678");
+    }
+
+    @Test
+    public void notEnoughParameters() throws Exception {
+        this.failOnThisNumberOfArguments();
+    }
+
+    @Test
+    public void tooManyParameters() throws Exception {
+        this.failOnThisNumberOfArguments(new BigDecimal(1), new BigDecimal(1));
+        this.failOnThisNumberOfArguments(new BigDecimal(1), new BigDecimal(1), new BigDecimal(1));
+        this.failOnThisNumberOfArguments(new BigDecimal(1), new BigDecimal(1), new BigDecimal(1), new BigDecimal(1));
+    }
+
+    @Test
+    public void operatorShouldBeValid() throws Exception {
+        checkOperatorToCalculate("sqrt", true);
+    }
+
+    @Test
+    public void operatorShouldBeInvalid() throws Exception {
+        for (String temp : Operators.listOfOperators) {
+            if (!temp.equals("sqrt")) {
+                checkOperatorToCalculate(temp, false);
+            }
+        }
+    }
+
+    private void failOnThisArgument(String leftParameter) throws Exception {
+        Squareroot squareroot = new Squareroot();
+        BigDecimal leftParameterBD = new BigDecimal(leftParameter);
+
+        expectedException.expect(ArithmeticException.class);
+
+        squareroot.calculate(leftParameterBD);
+    }
+
+    public void failOnThisNumberOfArguments(BigDecimal... arguments) throws Exception {
+        Squareroot squareroot = new Squareroot();
+
+        expectedException.expect(NumberOfParametersException.class);
+        expectedException.expectMessage(equalTo("Parameters: " + arguments.length));
+
+        squareroot.calculate(arguments);
+    }
+
+    public void SquarerootOfValuesIsCorrect(String... arguments) throws Exception {
+        Squareroot squareroot = new Squareroot();
+        BigDecimal calculate = squareroot.calculate(new BigDecimal(arguments[0]));
+        BigDecimal expected = new BigDecimal(arguments[1]);
+        String message = "\nExpected: " + expected.toString() + "\nActual: " + calculate.toString();
+
+        assertEquals(message, 0, calculate.compareTo(expected));
+    }
+
+    public void checkOperatorToCalculate(String operator, boolean shouldBeCorrect) throws Exception {
+        Squareroot squareroot = new Squareroot();
+        String message = "Operator: " + operator;
+        if (shouldBeCorrect) {
+            assertTrue(message, squareroot.canCalculate(operator));
+        }
+        else {
+            assertFalse(message, squareroot.canCalculate(operator));
+        }
+    }
+}
